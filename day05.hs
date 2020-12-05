152208c4cd4964b86200e1c80a9d00575e176334
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-05 11:44:39 +0200

    talk about partitions, regions and seat ids

--- day05.hs
+++ day05.hs
@@ -1,33 +1,32 @@
 import Data.Bifunctor
 import Data.List
 
-reduceSeat :: Char -> (Int, Int) -> [Char] -> (Int, Int)
-reduceSeat pickUpper =
-  foldl
-    ( \range rule ->
-        if rule == pickUpper
-          then first (half ceiling . (+ snd range)) range
-          else second (half floor . (+ fst range)) range
-    )
+reducePartitions :: Char -> (Int, Int) -> [Char] -> Int
+reducePartitions pickUpper initialRegion partitions =
+  fst $ foldl cropRegion initialRegion partitions
   where
     half f x = f (fromIntegral x / 2)
+    cropRegion range partition =
+      if partition == pickUpper
+        then first (half ceiling . (+ snd range)) range
+        else second (half floor . (+ fst range)) range
 
-calcRid :: String -> Int
-calcRid s = row * 8 + col
+calcSid :: String -> Int
+calcSid s = row * 8 + col
   where
-    row = fst $ reduceSeat 'B' (0, 127) (take 7 s)
-    col = snd (reduceSeat 'R' (0, 7) (drop 7 s))
+    row = reducePartitions 'B' (0, 127) (take 7 s)
+    col = reducePartitions 'R' (0, 7) (drop 7 s)
 
 part1 :: [String] -> String
-part1 = (++) "Part 1: " <$> show . maximum . map calcRid
+part1 = (++) "Part 1: " <$> show . maximum . map calcSid
 
 part2 :: [String] -> String
 part2 lines =
   "Part 2: "
     ++ maybe "no seat" show ((+ 1) . fst <$> find (\(a, b) -> abs (a - b) == 2) seatPairs)
   where
-    ids = sort $ map calcRid lines
-    seatPairs = zip ids (drop 1 ids)
+    sids = sort $ map calcSid lines
+    seatPairs = zip sids (drop 1 sids)
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . lines)
