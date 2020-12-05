815adfef98966436c72ef1bcee61c952f93de076
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-05 10:52:52 +0200

    solution for day 5

---
+++ day05.hs
@@ -0,0 +1,32 @@
+import Data.List
+
+reducePlace :: Char -> (Int, Int) -> [Char] -> (Int, Int)
+reducePlace pickUpper =
+  foldl
+    ( \range rule ->
+        if rule == pickUpper
+          then (half ceiling (snd range + fst range), snd range)
+          else (fst range, half floor (snd range + fst range))
+    )
+  where
+    half f x = f (fromIntegral x / 2)
+
+calcRid :: String -> Int
+calcRid s = row * 8 + col
+  where
+    row = fst $ reducePlace 'B' (0, 127) (take 7 s)
+    col = snd (reducePlace 'R' (0, 7) (drop 7 s))
+
+part1 :: [String] -> String
+part1 = (++) "Part 1: " <$> show . maximum . map calcRid
+
+part2 :: [String] -> String
+part2 lines =
+  "Part 2: "
+    ++ maybe "no seat" show ((+ 1) . fst <$> find (\(a, b) -> abs (a - b) == 2) seatPairs)
+  where
+    ids = sort $ map calcRid lines
+    seatPairs = zip ids (drop 1 ids)
+
+main :: IO ()
+main = interact (unlines . sequence [part1, part2] . lines)
