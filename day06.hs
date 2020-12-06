779c80650c24abb63d88dc5fc45400d085e83cab
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-06 15:47:36 +0200

    swap sum map length to length and concatMap

--- day06.hs
+++ day06.hs
@@ -2,10 +2,10 @@ import Data.List
 import Data.List.Split (splitOn)
 
 part1 :: [String] -> String
-part1 = (++) "Part 1: " . show . sum . map (length . nub . filter (/= '\n'))
+part1 = (++) "Part 1: " . show . length . concatMap (nub . filter (/= '\n'))
 
 part2 :: [String] -> String
-part2 = (++) "Part 2: " . show . sum . map (length . foldl1 intersect . lines)
+part2 = (++) "Part 2: " . show . length . concatMap (foldl1 intersect . lines)
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . splitOn "\n\n")
