347b24f3ce5f8b835507a1ff664d1f1630fd0769
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-06 13:26:11 +0200

    refactor and shorter day 6

--- day06.hs
+++ day06.hs
@@ -2,10 +2,10 @@ import Data.List
 import Data.List.Split
 
 part1 :: [String] -> String
-part1 groups = "Part 1: " ++ show (sum $ map (length . nub . filter (/= '\n')) groups)
+part1 = (++) "Part 1: " . show . sum <$> map (length . nub . filter (/= '\n'))
 
 part2 :: [String] -> String
-part2 groups = "Part 2: " ++ show (sum $ map (length . foldl1 intersect . lines) groups)
+part2 = (++) "Part 2: " . show . sum <$> map (length . foldl1 intersect . lines)
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . splitOn "\n\n")
