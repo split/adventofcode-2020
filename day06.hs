a73758cd7f7ea5128bd9e09a680cd41e4c795895
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-06 15:27:27 +0200

    remove fmap

--- day06.hs
+++ day06.hs
@@ -1,11 +1,11 @@
 import Data.List
-import Data.List.Split
+import Data.List.Split (splitOn)
 
 part1 :: [String] -> String
-part1 = (++) "Part 1: " . show . sum <$> map (length . nub . filter (/= '\n'))
+part1 = (++) "Part 1: " . show . sum . map (length . nub . filter (/= '\n'))
 
 part2 :: [String] -> String
-part2 = (++) "Part 2: " . show . sum <$> map (length . foldl1 intersect . lines)
+part2 = (++) "Part 2: " . show . sum . map (length . foldl1 intersect . lines)
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . splitOn "\n\n")
