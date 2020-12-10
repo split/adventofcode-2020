5a1245a5b5908bf26c26e61b4f532b3ccea98f6b
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-10 14:20:37 +0200

    day 10 solution

---
+++ day10.hs
@@ -0,0 +1,24 @@
+import Data.List
+
+part1 :: [Int] -> String
+part1 adapters = "Part 1: " ++ show (productDiffs diffs)
+  where
+    productDiffs = product . map length . group . filter (/= 2)
+    diffs = sort (zipWith (-) (drop 1 adapters) adapters)
+
+countArranges :: [Int] -> [Int]
+countArranges [] = []
+countArranges (rating : restRatings) = count : nextCounts
+  where
+    nextCounts = countArranges restRatings
+    connectable = takeWhile (<= rating + 3) restRatings
+    count = max 1 $ sum $ take (length connectable) nextCounts
+
+part2 :: [Int] -> String
+part2 ratings = "Part 2: " ++ show (head $ countArranges ratings)
+
+withOutlitAndBuiltIn :: Num a => [a] -> [a]
+withOutlitAndBuiltIn ratings = 0 : ratings ++ [last ratings + 3]
+
+main :: IO ()
+main = interact (unlines . sequence [part1, part2] . withOutlitAndBuiltIn . sort . map read . lines)
