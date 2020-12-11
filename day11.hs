068e21b5469e6ce28ec473d5fc39f9d437d8e6d9
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-11 16:12:25 +0200

    optimize day 11 to use Map

--- day11.hs
+++ day11.hs
@@ -1,9 +1,12 @@
 import Control.Monad
+import Data.List (drop, filter, map)
+import qualified Data.Map as Map
 import Data.Maybe (isJust, mapMaybe)
+import Prelude hiding (drop, filter, lookup, map)
 
 type Coord = (Int, Int)
 
-type Coordinated a = (Coord, a)
+type Plane = Map.Map Coord Char
 
 dirs :: [Coord]
 dirs = [(x, y) | x <- [(-1) .. 1], y <- [(-1) .. 1], (x, y) /= (0, 0)]
@@ -11,16 +14,16 @@ dirs = [(x, y) | x <- [(-1) .. 1], y <- [(-1) .. 1], (x, y) /= (0, 0)]
 sumCoord :: Coord -> Coord -> Coord
 sumCoord (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
-coordinated :: [String] -> [Coordinated Char]
-coordinated lines = [((x, y), c) | (y, ys) <- zip [0 ..] lines, (x, c) <- zip [0 ..] ys]
+coordinated :: [String] -> Plane
+coordinated lines = Map.fromList [((x, y), c) | (y, ys) <- zip [0 ..] lines, (x, c) <- zip [0 ..] ys]
 
-adjacent :: [Coordinated Char] -> Coord -> String
-adjacent plane seat = mapMaybe ((`lookup` plane) . sumCoord seat) dirs
+adjacent :: Plane -> Coord -> String
+adjacent plane seat = mapMaybe ((`Map.lookup` plane) . sumCoord seat) dirs
 
-seeable :: [Coordinated Char] -> Coord -> String
+seeable :: Plane -> Coord -> String
 seeable plane seat = mapMaybe see dirs
   where
-    see dir = msum $ filter (/= Just '.') $ takeWhile isJust $ map (`lookup` plane) (pointsInDir dir)
+    see dir = msum $ filter (/= Just '.') $ takeWhile isJust $ map (`Map.lookup` plane) (pointsInDir dir)
     pointsInDir dir = iterate (sumCoord dir) (sumCoord dir seat)
 
 createRule :: Int -> String -> Char -> Char
@@ -29,22 +32,22 @@ createRule limit adj seat
   | seat == '#' && length (filter (== '#') adj) >= limit = 'L'
   | otherwise = seat
 
-gameOfPlane :: ([Coordinated Char] -> Coordinated Char -> Coordinated Char) -> [Coordinated Char] -> Int
+gameOfPlane :: (Plane -> Coord -> Char -> Char) -> Plane -> Int
 gameOfPlane rule plane = occupied !! length notRepeating
   where
     notRepeating = takeWhile (/= True) $ zipWith (==) occupied (drop 1 occupied)
-    occupied = countOccupied <$> iterate (\p -> map (rule p) p) plane
-    countOccupied = length . filter ((== '#') . snd)
+    occupied = countOccupied <$> iterate (\plane' -> Map.mapWithKey (rule plane') plane') plane
+    countOccupied = length . Map.filter (== '#')
 
-part1 :: [Coordinated Char] -> String
+part1 :: Plane -> String
 part1 = (++) "Part 1: " . show . gameOfPlane rule
   where
-    rule plane (p, s) = (p, createRule 4 (adjacent plane p) s)
+    rule plane p s = createRule 4 (adjacent plane p) s
 
-part2 :: [Coordinated Char] -> String
+part2 :: Plane -> String
 part2 = (++) "Part 2: " . show . gameOfPlane rule
   where
-    rule plane (p, s) = (p, createRule 5 (seeable plane p) s)
+    rule plane p s = createRule 5 (seeable plane p) s
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . coordinated . lines)
