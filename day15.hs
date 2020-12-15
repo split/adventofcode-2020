8edbee23b986e0d74e413d77b845e27857de4347
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-15 11:07:58 +0200

    cleanup day 15 solution

--- day15.hs
+++ day15.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 
@@ -9,17 +7,15 @@ getTurn :: Int -> [(Int, Int)] -> Int
 getTurn targetTurn initial = runGame targetTurn (Map.fromList $ init initial) (last initial)
 
 runGame :: Int -> GameHistory -> (Int, Int) -> Int
-runGame targetTurn hist lastNum =
+runGame targetTurn hist lastNum@(lastValue, lastTurn) =
   if turn == targetTurn then answer else runGame targetTurn nextHist (answer, turn)
   where
-    (answer, nextHist) = getAnswer lastNum hist
-    turn = snd lastNum + 1
+    answer = getAnswer lastNum hist
+    nextHist = Map.insert lastValue lastTurn hist
+    turn = lastTurn + 1
 
-getAnswer :: (Int, Int) -> GameHistory -> (Int, GameHistory)
-getAnswer (lastNum, lastTurn) hist = answer searchResult
-  where
-    searchResult = maybe 0 (lastTurn -) (lastNum `Map.lookup` hist)
-    answer = (,Map.insert lastNum lastTurn hist)
+getAnswer :: (Int, Int) -> GameHistory -> Int
+getAnswer (lastNum, lastTurn) hist = maybe 0 (lastTurn -) (lastNum `Map.lookup` hist)
 
 main :: IO ()
 main = interact (unlines . solve . flip zip [1 ..] . map read . splitOn ",")
