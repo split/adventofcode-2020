58052fc330e9655c0feec585a21e0e14f06a472f
Henri Lahtinen <henri.lahtinen@meruhealth.com>
2020-12-19 15:00:51 +0200

    Day 19 part 2 solution

--- day19.hs
+++ day19.hs
@@ -1,6 +1,5 @@
 import Control.Monad.Combinators.Expr (Operator (InfixL), makeExprParser)
 import qualified Data.Map as Map
-import Debug.Trace
 import Text.ParserCombinators.Parsec
 
 data Rule = Match Char | RefR Int | And Rule Rule | Or Rule Rule
@@ -8,33 +7,20 @@ data Rule = Match Char | RefR Int | And Rule Rule | Or Rule Rule
 
 type RuleMap = Map.Map Int Rule
 
-mergeO :: [String] -> [String] -> [String]
-mergeO a b = concatMap (\a' -> map (a' ++) b) a
-
-toMatch :: RuleMap -> [String]
-toMatch ruleMap = go (RefR 0)
+toMatch :: RuleMap -> String -> Bool
+toMatch ruleMap = elem "" . go (RefR 0)
   where
-    go (Match c) = [[c]]
-    go (RefR i) = go (ruleMap Map.! i)
-    go (And a b) = mergeO (go a) (go b)
-    go (Or a b) = go a ++ go b
-
-{-
-0: 1 2 | 2 3
-1: 2 3
-2: "b"
-3: "c"
-
-ca
--}
-
--- processRule :: Int -> RuleMap -> Rule
+    go _ "" = []
+    go (Match c) (c' : rest) = [rest | c' == c]
+    go (RefR i) s = go (ruleMap Map.! i) s
+    go (And a b) s = go a s >>= go b
+    go (Or a b) s = go a s ++ go b s
 
 solve :: (RuleMap, [String]) -> Int
 solve (ruleMap, messages) = length $ filter validator messages
   where
     validator :: String -> Bool
-    validator s = s `elem` toMatch ruleMap
+    validator = toMatch ruleMap
 
 main :: IO ()
 main = interact (either show (show . solve) . parse doc "")
@@ -43,7 +29,9 @@ doc :: Parser (RuleMap, [String])
 doc = do
   rules <- Map.fromList <$> manyTill indexedRule (try (char '\n'))
   messages <- many1 letter `sepBy` char '\n' <* eof
-  return (rules, messages)
+  return (Map.union extra rules, messages)
+  where
+    extra = Map.fromList [(8, Or (RefR 42) (And (RefR 42) (RefR 8))), (11, Or (And (RefR 42) (RefR 31)) (And (RefR 42) (And (RefR 11) (RefR 31))))]
 
 indexedRule :: Parser (Int, Rule)
 indexedRule = (,) <$> int <* string ": " <*> rule <* char '\n'
